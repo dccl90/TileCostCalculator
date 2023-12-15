d478ba05c7d7b78a52da87760fa6f321dcd0eba9
Dean Clarke <dclarke@twilio.com>
2023-12-15 12:42:36 +1030

    Fix #8 Update code to take correct input for triangle measurements and improved output formatting

--- Program.cs
+++ Program.cs
@@ -13,12 +13,13 @@ namespace TileCostCalculator // Note: actual namespace depends on the project na
             double flooringCost;
             double labourCost;
             double costPerUnit;
-            double length;
-            double width;
             int shape;
 
             Console.WriteLine("**** Tile Cost Calculator ****");
-            Console.Write("Enter 1 for a Rectangle Room or 2 for a Triangle Room: ");
+            Console.WriteLine("Enter 1 for a Rectangle Room");
+            Console.WriteLine("Enter 2 for a Triangle Room");
+            Console.Write(": ");
+
             shape = Convert.ToInt32(Console.ReadLine());
 
             if(shape < 1 | shape > 2)
@@ -27,30 +28,34 @@ namespace TileCostCalculator // Note: actual namespace depends on the project na
                 return;
             } 
 
-            Console.Write("Please enter the length of the room in feet: ");
-            length = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Please enter the width of the room in feet: ");
-            width = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Please enter the cost per square foot: ");
-            costPerUnit = Convert.ToDouble(Console.ReadLine());
-
             if(shape == 1)
             {
+                Console.Write("Please enter the length of the rectangle in feet: ");
+                double length = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Please enter the width of the rectangle in feet: ");
+                double width = Convert.ToDouble(Console.ReadLine());
                 area = length * width;
             }
 
             if(shape == 2)
             {  
-                area = 0.5 * (length * width);
+                Console.Write("Please enter the base of the triangle in feet: ");
+                double triangleBase = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Please enter the height of the triangle in feet: ");
+                double height = Convert.ToDouble(Console.ReadLine());
+                area = 0.5 * (triangleBase * height);
             }
+
+            Console.Write("Please enter the cost per square foot: ");
+            costPerUnit = Convert.ToDouble(Console.ReadLine());
 
             labourCost = area / SQUARE_METERS_PER_HOUR * LABOUR_RATE;
             flooringCost = area * costPerUnit;
             Console.WriteLine("----------------------------------");   
-            Console.WriteLine($"Room Area: {area} sq ft");
-            Console.WriteLine($"Flooring Cost: ${flooringCost}");
-            Console.WriteLine($"Labour Cost: ${labourCost}");
-            Console.WriteLine($"Total Cost: ${flooringCost + labourCost}");
+            Console.WriteLine($"Room Area: {area:.00} sq ft");
+            Console.WriteLine($"Flooring Cost: {flooringCost:C}");
+            Console.WriteLine($"Labour Cost: {labourCost:C}");
+            Console.WriteLine($"Total Cost: {flooringCost + labourCost:C}");
             Console.Write("----------------------------------");    
         }
     }
